50bc9c9dca86029dca4c94f91c76e4155f5956c5
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-06-23 20:43:14 -0600

    Add a radix tree data structure for configuration serialization.

---
+++ src/pwmc/config/ConfigurationRadixTree.hpp
@@ -0,0 +1,68 @@
+/*
+ * pwm - A simple password manager for Linux.
+ * Copyright (C) 2015  Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef pwmc_config_ConfigurationRadixTree_HPP
+#define pwmc_config_ConfigurationRadixTree_HPP
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "pwmc/config/Configuration.hpp"
+
+namespace pwm
+{
+namespace config
+{
+namespace detail
+{
+struct ConfigurationRadixTreeNode
+{
+	std::string key;
+
+	std::vector<std::unique_ptr<ConfigurationRadixTreeNode>> children;
+	std::string value;
+
+	ConfigurationRadixTreeNode(const std::string& k);
+
+	ConfigurationRadixTreeNode(const ConfigurationRadixTreeNode&) = delete;
+	~ConfigurationRadixTreeNode() = default;
+	ConfigurationRadixTreeNode& operator=(const ConfigurationRadixTreeNode&) = delete;
+};
+}
+
+class ConfigurationRadixTree
+{
+public:
+	typedef std::function<bool(const std::string &k, const std::string &v)> TraversalFunction;
+
+	ConfigurationRadixTree(const ConfigurationData& data);
+
+	~ConfigurationRadixTree() = default;
+
+	void traverse(const TraversalFunction &pre,
+		const TraversalFunction &post) const;
+
+private:
+	std::unique_ptr<detail::ConfigurationRadixTreeNode> root;
+};
+}
+}
+
+#endif
