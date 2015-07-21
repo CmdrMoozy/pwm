b1026741eeb113f8572930e4705474e922b21d03
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-07-20 20:59:45 -0600

    Add a utility for suppressing std::c{out,err} output.

---
+++ src/pwmc/util/OutputSuppressor.hpp
@@ -0,0 +1,50 @@
+/*
+ * pwm - A simple password manager for Linux.
+ * Copyright (C) 2015  Axel Rasmussen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef pwmc_util_OutputSuppressor_HPP
+#define pwmc_util_OutputSuppressor_HPP
+
+#include <iostream>
+#include <sstream>
+
+namespace pwm
+{
+namespace util
+{
+class OutputSuppressor
+{
+public:
+	OutputSuppressor();
+
+	OutputSuppressor(OutputSuppressor const&) = delete;
+
+	~OutputSuppressor();
+
+	OutputSuppressor& operator=(OutputSuppressor const&) = delete;
+
+private:
+	std::ostringstream out;
+	std::ostringstream err;
+
+	decltype(std::cout.rdbuf()) originalOut;
+	decltype(std::cerr.rdbuf()) originalErr;
+};
+}
+}
+
+#endif
