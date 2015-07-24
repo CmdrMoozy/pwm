82eb58644c8b1ea70f5bd1b2dc0953c9c7304180
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-07-23 23:51:55 -0600

    Add Argument list validation to Command and associated unit tests.

--- src/pwmc/params/Command.cpp
+++ src/pwmc/params/Command.cpp
@@ -18,6 +18,9 @@
 
 #include "Command.hpp"
 
+#include <algorithm>
+#include <stdexcept>
+
 namespace pwm
 {
 namespace params
@@ -31,6 +34,28 @@ Command::Command(std::string const &n, std::string const &h,
           arguments(a),
           lastArgumentIsVariadic(laiv)
 {
+	// After the first argument with a default value, all other arguments
+	// must also have default values (just like in C++).
+	auto firstDefault = std::find_if(arguments.begin(), arguments.end(),
+	                                 [](Argument const &argument) -> bool
+	                                 {
+		                                 return !!argument.defaultValue;
+		                         });
+	auto lastNonDefault =
+	        std::find_if(arguments.rbegin(), arguments.rend(),
+	                     [](Argument const &argument) -> bool
+	                     {
+		                     return !argument.defaultValue;
+		             }).base();
+	if(lastNonDefault != arguments.end()) --lastNonDefault;
+
+	if(firstDefault < lastNonDefault)
+	{
+		throw std::runtime_error("Invalid command; after the first "
+		                         "argument with a default value, all "
+		                         "other arguments must also have "
+		                         "default values.");
+	}
 }
 
 bool operator<(Command const &a, Command const &b)
