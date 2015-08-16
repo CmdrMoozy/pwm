d86645abd57a48d48ac8d489025a05bb3a370640
Axel Rasmussen <axel.rasmussen1@gmail.com>
2015-08-16 10:47:50 -0600

    Update clang-format script to include header files.

--- format.sh
+++ format.sh
@@ -5,7 +5,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-find src/ -type f -iname "*.h" -or -iname "*.cpp" | while read FILE
+find src/ -type f -iname "*.hpp" -or -iname "*.cpp" | while read FILE
 do
 	clang-format -i "$FILE"
 done
