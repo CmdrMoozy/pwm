2f597e0a1096e4089d582e870a4e6145205ce1c1
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-12-31 10:10:15 -0800

    Add initialization hook for sodiumoxide.

---
+++ src/error.rs
@@ -0,0 +1,68 @@
+// pwm - A simple password manager for Linux.
+// Copyright (C) 2015  Axel Rasmussen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use backtrace::Backtrace;
+use std::cmp::{Eq, PartialEq};
+use std::error;
+use std::fmt;
+use std::result;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum ErrorKind {
+    Initialization { cause: String },
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub kind: ErrorKind,
+    pub backtrace: Backtrace,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind) -> Error {
+        Error {
+            kind: kind,
+            backtrace: Backtrace::new(),
+        }
+    }
+}
+
+impl PartialEq for Error {
+    fn eq(&self, other: &Error) -> bool { self.kind == other.kind }
+}
+
+impl Eq for Error {}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match self.kind {
+            ErrorKind::Initialization { cause: _ } => "Library initialization error",
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use std::error::Error;
+        match self.kind {
+            ErrorKind::Initialization { cause: ref c } => {
+                f.write_str(format!("{}: {}", self.description(), c).as_str())
+            },
+        }
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
