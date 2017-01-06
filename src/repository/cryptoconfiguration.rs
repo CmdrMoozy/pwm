875288407cae83f6299aadd27b95a7dff62e2384
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-01-06 00:12:22 -0800

    Add very basic crypto configuration support to Repository.

--- src/repository/cryptoconfiguration.rs
+++ src/repository/cryptoconfiguration.rs
@@ -14,7 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use bdrck_config::configuration as bdrck_config;
+use ::error::Result;
+use sodiumoxide::crypto::pwhash;
 use sodiumoxide::crypto::pwhash::{MemLimit, OpsLimit, Salt, SALTBYTES};
+use std::path::Path;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CryptoConfiguration {
@@ -29,4 +33,48 @@ impl CryptoConfiguration {
     pub fn get_mem_limit(&self) -> MemLimit { MemLimit(self.mem_limit) }
 
     pub fn get_ops_limit(&self) -> OpsLimit { OpsLimit(self.ops_limit) }
+}
+
+lazy_static! {
+    static ref DEFAULT_CRYPTO_CONFIGURATION: CryptoConfiguration = CryptoConfiguration {
+        salt: pwhash::gen_salt().0,
+        mem_limit: pwhash::MEMLIMIT_INTERACTIVE.0,
+        ops_limit: pwhash::OPSLIMIT_INTERACTIVE.0,
+    };
+}
+
+pub struct CryptoConfigurationInstance {
+    identifier: bdrck_config::Identifier,
+}
+
+impl CryptoConfigurationInstance {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<CryptoConfigurationInstance> {
+        let instance = CryptoConfigurationInstance {
+            identifier: bdrck_config::Identifier {
+                application: "pwm".to_owned(),
+                name: path.as_ref().to_string_lossy().into_owned(),
+            },
+        };
+        try!(bdrck_config::new(instance.identifier.clone(),
+                               DEFAULT_CRYPTO_CONFIGURATION.clone(),
+                               Some(path.as_ref())));
+        Ok(instance)
+    }
+
+    pub fn close(self) -> Result<()> {
+        try!(bdrck_config::remove::<CryptoConfiguration>(&self.identifier));
+        Ok(())
+    }
+
+    pub fn get(&self) -> Result<CryptoConfiguration> {
+        Ok(try!(bdrck_config::get::<CryptoConfiguration>(&self.identifier)))
+    }
+
+    pub fn set(&self, config: CryptoConfiguration) -> Result<()> {
+        Ok(try!(bdrck_config::set(&self.identifier, config)))
+    }
+
+    pub fn reset(&self) -> Result<()> {
+        Ok(try!(bdrck_config::reset::<CryptoConfiguration>(&self.identifier)))
+    }
 }
--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod cryptoconfiguration;
-
+mod cryptoconfiguration;
 mod repository;
 
+pub use ::repository::cryptoconfiguration::{CryptoConfiguration, CryptoConfigurationInstance};
 pub use ::repository::repository::Repository;
