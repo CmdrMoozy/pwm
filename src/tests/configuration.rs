9c0977e30c37c9b6288a86734a9c7841efbee5a7
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-01-28 11:01:27 -0800

    Serialize config tests, since they access a global singleton.

--- src/tests/configuration.rs
+++ src/tests/configuration.rs
@@ -17,10 +17,22 @@
 use ::configuration::*;
 use std::fs;
 use std::path::PathBuf;
+use std::sync::Mutex;
 use ::tests::tempfile;
+
+lazy_static! {
+    // The unit tests in this file MUST be run one-at-a-time, since they all access our
+    // global singleton configuration state.
+    static ref CONFIGURATION_TESTS_MUTEX: Mutex<()> = Mutex::new(());
+}
 
 #[test]
 fn test_get_and_set() {
+    let _guard = match CONFIGURATION_TESTS_MUTEX.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    };
+
     let file = tempfile::NamedTempFile::new().ok().unwrap();
     let path: PathBuf = file.path().to_owned();
     // Remove the file: an empty file isn't a valid serialized configuration struct.
@@ -46,6 +58,11 @@ fn test_get_and_set() {
 
 #[test]
 fn test_get_value_as_str() {
+    let _guard = match CONFIGURATION_TESTS_MUTEX.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    };
+
     let file = tempfile::NamedTempFile::new().ok().unwrap();
     let path: PathBuf = file.path().to_owned();
     // Remove the file: an empty file isn't a valid serialized configuration struct.
@@ -71,6 +88,11 @@ fn test_get_value_as_str() {
 
 #[test]
 fn test_reset() {
+    let _guard = match CONFIGURATION_TESTS_MUTEX.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    };
+
     let file = tempfile::NamedTempFile::new().ok().unwrap();
     let path: PathBuf = file.path().to_owned();
     // Remove the file: an empty file isn't a valid serialized configuration struct.
