760216542e8d007ac7d8f662351294f75c4d67aa
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-03-12 16:34:00 -0700

    Modify Repository to use KeyStore for the master key.
    This requires us to remove the capability to modify crypto
    configuration. The intent is to redesign / readd this functionality
    later.

--- src/repository/repository.rs
+++ src/repository/repository.rs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use crypto::key::Key;
+use crypto::keystore::KeyStore;
 use crypto::padding;
 use error::Result;
 use git2;
@@ -30,15 +31,13 @@ use util::password_prompt;
 
 lazy_static! {
     static ref CRYPTO_CONFIGURATION_PATH: PathBuf = PathBuf::from("crypto_configuration.mp");
-    static ref AUTH_TOKEN_PATH: PathBuf = PathBuf::from(".auth_token");
-
-    static ref AUTH_TOKEN_CONTENTS: Vec<u8> =
-        "3c017f717b39247c351154a41d2850e4187284da4b928f13c723d54440ba2dfe".bytes().collect();
+    static ref KEYSTORE_PATH: PathBuf = PathBuf::from("keys.mp");
 }
 
 static MASTER_PASSWORD_PROMPT: &'static str = "Master password: ";
 
 static CRYPTO_CONFIGURATION_UPDATE_MESSAGE: &'static str = "Update encryption header contents.";
+static KEYSTORE_UPDATE_MESSAGE: &'static str = "Update keys.";
 static STORED_PASSWORD_UPDATE_MESSAGE: &'static str = "Update stored password / key.";
 static STORED_PASSWORD_REMOVE_MESSAGE: &'static str = "Remove stored password / key.";
 
@@ -53,30 +52,24 @@ fn open_crypto_configuration(repository: &git2::Repository) -> Result<Configurat
     ConfigurationInstance::new(path.as_path())
 }
 
-fn write_encrypt(repository: &git2::Repository,
-                 path: &RepositoryPath,
-                 plaintext: SensitiveData,
-                 master_key: &Key)
-                 -> Result<()> {
+fn open_keystore(repository: &git2::Repository, master_key: &Key) -> Result<KeyStore> {
+    let mut path = PathBuf::from(try!(git::get_repository_workdir(repository)));
+    path.push(KEYSTORE_PATH.as_path());
+    KeyStore::open_or_new(path.as_path(), &master_key)
+}
+
+fn write_encrypt(path: &RepositoryPath, plaintext: SensitiveData, master_key: &Key) -> Result<()> {
     let (nonce, data) = try!(master_key.encrypt(padding::pad(plaintext)));
 
     if let Some(parent) = path.absolute_path().parent() {
         try!(fs::create_dir_all(parent));
     }
 
-    {
-        use std::io::Write;
-        let mut file = try!(File::create(path.absolute_path()));
-        try!(file.write_all(&nonce.0));
-        try!(file.write_all(data.as_slice()));
-        try!(file.flush());
-    }
-
-    try!(git::commit_paths(&repository,
-                           Some(&get_commit_signature(repository)),
-                           Some(&get_commit_signature(repository)),
-                           STORED_PASSWORD_UPDATE_MESSAGE,
-                           &[PathBuf::from(path.relative_path()).as_path()]));
+    use std::io::Write;
+    let mut file = try!(File::create(path.absolute_path()));
+    try!(file.write_all(&nonce.0));
+    try!(file.write_all(data.as_slice()));
+    try!(file.flush());
     Ok(())
 }
 
@@ -97,55 +90,12 @@ fn read_decrypt(path: &RepositoryPath, master_key: &Key) -> Result<SensitiveData
     padding::unpad(try!(master_key.decrypt(data.as_slice(), &nonce)))
 }
 
-fn write_auth_token(repository: &git2::Repository, master_key: &Key) -> Result<()> {
-    write_encrypt(repository,
-                  &try!(RepositoryPath::new(try!(git::get_repository_workdir(repository)),
-                                            AUTH_TOKEN_PATH.as_path())),
-                  SensitiveData::from(AUTH_TOKEN_CONTENTS.clone()),
-                  master_key)
-}
-
-fn verify_auth_token(repository: &git2::Repository, create: bool, master_key: &Key) -> Result<()> {
-    let mut path = PathBuf::from(try!(git::get_repository_workdir(repository)));
-    path.push(AUTH_TOKEN_PATH.as_path());
-
-    if create && !path.exists() {
-        try!(write_auth_token(repository, master_key));
-    }
-
-    if let Ok(token) =
-        read_decrypt(&try!(RepositoryPath::new(try!(git::get_repository_workdir(repository)),
-                                               AUTH_TOKEN_PATH.as_path())),
-                     master_key) {
-        if &token[..] == &AUTH_TOKEN_CONTENTS[..] {
-            return Ok(());
-        }
-    }
-
-    bail!("Incorrect master password");
-}
-
-fn reencrypt_all(repository: &git2::Repository,
-                 listing: &[RepositoryPath],
-                 old_master_key: &Key,
-                 new_master_key: &Key)
-                 -> Result<()> {
-    for path in listing {
-        try!(write_encrypt(repository,
-                           path,
-                           try!(read_decrypt(path, old_master_key)),
-                           new_master_key));
-    }
-    try!(write_auth_token(repository, new_master_key));
-    Ok(())
-}
-
 pub struct Repository {
     repository: git2::Repository,
     // NOTE: crypto_configuration is guaranteed to be Some() everywhere except within drop().
     crypto_configuration: Option<ConfigurationInstance>,
-    master_password: SensitiveData,
-    master_key: Key,
+    // NOTE: keystore is guaranteed to be Some() everywhere except within drop().
+    keystore: Option<KeyStore>,
 }
 
 impl Repository {
@@ -163,15 +113,13 @@ impl Repository {
         } else {
             try!(password_prompt(MASTER_PASSWORD_PROMPT, create))
         };
-        let master_key = try!(crypto_configuration.get().build_key(master_password.clone()));
-
-        try!(verify_auth_token(&repository, create, &master_key));
+        let master_key = try!(crypto_configuration.get().build_key(master_password));
+        let keystore = try!(open_keystore(&repository, &master_key));
 
         Ok(Repository {
             repository: repository,
             crypto_configuration: Some(crypto_configuration),
-            master_password: master_password,
-            master_key: master_key,
+            keystore: Some(keystore),
         })
     }
 
@@ -183,20 +131,22 @@ impl Repository {
         self.crypto_configuration.as_ref().unwrap().get()
     }
 
-    pub fn set_crypto_configuration(&mut self, config: Configuration) -> Result<()> {
-        self.crypto_configuration.as_ref().unwrap().set(config.clone());
-        let master_password = self.master_password.clone();
-        self.reencrypt(try!(config.build_key(master_password)))
-    }
-
-    pub fn reset_crypto_configuration(&mut self) -> Result<()> {
-        self.crypto_configuration.as_ref().unwrap().reset();
-        let crypto_configuration = self.get_crypto_configuration();
-        let master_password = self.master_password.clone();
-        self.reencrypt(try!(crypto_configuration.build_key(master_password)))
-    }
+    fn get_master_key(&self) -> &Key { self.keystore.as_ref().unwrap().get_key() }
 
     pub fn workdir(&self) -> Result<&Path> { git::get_repository_workdir(&self.repository) }
+
+    fn commit_all(&self, message: &str, paths: &[&Path]) -> Result<()> {
+        try!(git::commit_paths(&self.repository,
+                               Some(&get_commit_signature(&self.repository)),
+                               Some(&get_commit_signature(&self.repository)),
+                               message,
+                               paths));
+        Ok(())
+    }
+
+    fn commit_one(&self, message: &str, path: &Path) -> Result<()> {
+        self.commit_all(message, &[path])
+    }
 
     pub fn list(&self, path_filter: Option<&RepositoryPath>) -> Result<Vec<RepositoryPath>> {
         let default_path_filter = try!(self.path(""));
@@ -205,51 +155,36 @@ impl Repository {
                                                        path_filter.relative_path()));
         entries.into_iter()
             .filter(|entry| entry != CRYPTO_CONFIGURATION_PATH.as_path())
-            .filter(|entry| entry != AUTH_TOKEN_PATH.as_path())
+            .filter(|entry| entry != KEYSTORE_PATH.as_path())
             .map(|entry| self.path(entry))
             .collect()
     }
 
     pub fn write_encrypt(&self, path: &RepositoryPath, plaintext: SensitiveData) -> Result<()> {
-        write_encrypt(&self.repository, path, plaintext, &self.master_key)
+        try!(write_encrypt(path, plaintext, self.get_master_key()));
+        try!(self.commit_one(STORED_PASSWORD_UPDATE_MESSAGE, path.relative_path()));
+        Ok(())
     }
 
     pub fn read_decrypt(&self, path: &RepositoryPath) -> Result<SensitiveData> {
-        read_decrypt(path, &self.master_key)
+        read_decrypt(path, self.get_master_key())
     }
 
     pub fn remove(&self, path: &RepositoryPath) -> Result<()> {
         try!(fs::remove_file(path.absolute_path()));
-        try!(git::commit_paths(&self.repository,
-                               Some(&get_commit_signature(&self.repository)),
-                               Some(&get_commit_signature(&self.repository)),
-                               STORED_PASSWORD_REMOVE_MESSAGE,
-                               &[path.relative_path()]));
-        Ok(())
-    }
-
-    fn reencrypt(&mut self, new_master_key: Key) -> Result<()> {
-        {
-            let old_master_key = &self.master_key;
-            try!(reencrypt_all(&self.repository,
-                               &try!(self.list(None)),
-                               old_master_key,
-                               &new_master_key));
-        }
-
-        self.master_key = new_master_key;
+        try!(self.commit_one(STORED_PASSWORD_REMOVE_MESSAGE, path.relative_path()));
         Ok(())
     }
 }
 
 impl Drop for Repository {
     fn drop(&mut self) {
+        self.keystore.take();
+        self.commit_one(KEYSTORE_UPDATE_MESSAGE, KEYSTORE_PATH.as_path()).unwrap();
+
         self.crypto_configuration.take().unwrap().close().unwrap();
-        git::commit_paths(&self.repository,
-                          Some(&get_commit_signature(&self.repository)),
-                          Some(&get_commit_signature(&self.repository)),
-                          CRYPTO_CONFIGURATION_UPDATE_MESSAGE,
-                          &[CRYPTO_CONFIGURATION_PATH.as_path()])
+        self.commit_one(CRYPTO_CONFIGURATION_UPDATE_MESSAGE,
+                        CRYPTO_CONFIGURATION_PATH.as_path())
             .unwrap();
     }
 }
