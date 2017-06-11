47707b1ef4e2d5cbd5a0380bc09c9a06a5e59fed
Axel Rasmussen <axel.rasmussen1@gmail.com>
2017-06-10 23:51:12 -0700

    Bump dependency versions.

--- src/util/serde.rs
+++ src/util/serde.rs
@@ -14,7 +14,8 @@
 
 use bincode::{self, deserialize, serialize};
 use error::Result;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
+use serde::de::DeserializeOwned;
 
 pub fn serialize_binary<T: Serialize>(data: &T) -> Result<Vec<u8>> {
     match serialize(data, bincode::Infinite) {
@@ -23,7 +24,7 @@ pub fn serialize_binary<T: Serialize>(data: &T) -> Result<Vec<u8>> {
     }
 }
 
-pub fn deserialize_binary<T: Deserialize>(data: &[u8]) -> Result<T> {
+pub fn deserialize_binary<T: DeserializeOwned>(data: &[u8]) -> Result<T> {
     match deserialize(data) {
         Err(e) => bail!("Binary deserialization failed: {}", e),
         Ok(d) => Ok(d),
